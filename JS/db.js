4fc21cbe5c670fcaa2ca251bfd2cb06c4acd1ea8
Christian Platzer <christian@rainbow-solutions.net>
2013-03-12 11:25:10 +0100

    df

---
+++ JS/db.js
@@ -0,0 +1,8 @@
+function dbinit(){
+	StackMob.init({
+	    appName: "isports_10",
+	    clientSubdomain: "infoedvinfotechcom",
+	    publicKey: "3949c6ad-30d1-4e77-8b10-a426d38cd21e",
+	    apiVersion: 0
+	});
+}
